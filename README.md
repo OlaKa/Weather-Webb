b0771dacdf5034bcf5cb7de9f828c0d534cfb4a0
Ola Karlsson <ola.karlsson@bahnhof.se>
2023-03-02 21:39:31 +0100

    Updated API to work again
    Updated with weatherstack since  apixu is depracated.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Is a repository with different webapplications using public APIs, HTML, CSS and Javascript. 
 
-For the Weather Webbapp the API is from Apixu.com
+For the Weather Webbapp the API is from Weatherstack.com
 
 ~~To run this small app just type "python -m SimpleHTTPServer 8080" in a shell. If you dont have Python installed, install it first.~~
--- weather/script.js
+++ weather/script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "http://api.apixu.com/v1/forecast.json?key=9c8a9548c091422cae8223737170203&q=Stockholm&days=5", true);
+	xhttp.open("GET", "http://api.weatherstack.com/current?access_key=189421295101c742458fa8c625079742&query=Stockholm&forecast_days=5&hourly=1", true);
 	xhttp.onreadystatechange = getNewWeather;
 
 	xhttp.send();
@@ -9,21 +9,14 @@ window.onload = function() {
 
 function getNewWeather() {
 	if(this.readyState == 4 && this.status == 200) {
-		var weather = JSON.parse(this.responseText);
-		var tableList = document.getElementsByTagName("tbody")[0];
-		for(var i=0; i < weather.forecast.forecastday.length; i++) {
-			var newWeather = weather.forecast.forecastday[i];
+			var weather = JSON.parse(this.responseText);
+			var tableList = document.getElementsByTagName("tbody")[0];
 
 			tableList.innerHTML += "<tr>" +
-			"<td>" +newWeather.date + "</td>" +
-			"<td>"+ weather.location.region +"</td>" + 
-			"<td>"+ newWeather.day.avgtemp_c +" C</td>" + 
-			"<td><img src='http:" + newWeather.day.condition.icon + "'></td>" +
-			"</tr>";
-		}
+			"<td>" +weather.location.localtime + "</td>" +
+			"<td>"+ weather.location.region +"</td>" +
+			"<td>"+ weather.current.temperature +" C</td>" +
+			"<td>"+weather.current.weather_descriptions + "</td>" +
+			"</tr>"
 	}
-
 }
-
-
-
